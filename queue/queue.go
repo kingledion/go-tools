03cce77ddb2fed543d1f30a870c1a7219428bc96
daniel.hartig <kingledion@protonmail.com>
2022-04-03 20:52:55 -0500

    Implemented resizing in queue

--- queue/queue.go
+++ queue/queue.go
@@ -1,17 +1,35 @@
 package queue
 
+import "sync"
+
 type queue struct {
-	q chan interface{}
+	q         chan interface{}
+	capacity  int
+	expanding *sync.Mutex
 }
 
-func new(capacity int) *queue {
+func new() *queue {
+
 	return &queue{
-		q: make(chan interface{}, capacity),
+		q:         make(chan interface{}, 2),
+		capacity:  2,
+		expanding: &sync.Mutex{},
 	}
 }
 
 func (q *queue) push(elem interface{}) {
-	q.q <- elem
+	select {
+	case q.q <- elem:
+	default:
+		// resize the channel
+		q.capacity = q.capacity * 2
+		oldChan := q.q
+		q.q = make(chan interface{}, q.capacity)
+		for v := range *oldChan {
+			q.q <- v
+		}
+
+	}
 }
 
 func (q *queue) pop() interface{} {
